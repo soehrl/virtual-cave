7f71adc21709db306aaf545bacc29ed4c6754e8b
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-09-30 10:40:02 +0200

    feat: make dtrack uri configurable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from './Controls';
 import { Color, DoubleSide } from 'three';
+import { Box } from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
 
-function Box(props: any) {
+function Glasses(props: any) {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
   // Set up state for the hovered and active state
@@ -43,29 +45,15 @@ interface Data {
   bodies: Body[];
 };
 
-function Cave() {
+interface CaveProps {
+  glassPosition: [number, number, number];
+}
 
-  const ws = new WebSocket("ws://localhost:5000");
-
-  const [p, setP] = useState<[number, number, number]>([0, 0, 0]);
-
-  ws.onmessage = event => {
-    const d = JSON.parse(event.data) as Data;
-    if (d.bodies.length > 0) {
-      if (d.bodies[0].loc) {
-        setP([
-          d.bodies[0].loc[0] / 1000,
-          d.bodies[0].loc[1] / 1000,
-          d.bodies[0].loc[2] / 1000,
-        ]);
-      }
-    }
-  }
-
+function Cave(props: CaveProps) {
 
   return (
     <>
-      <Box position={p} />
+      <Glasses position={props.glassPosition} />
       <mesh rotation={[Math.PI / 2, 0, 0]}>
         <planeGeometry args={[caveSideLength, caveSideLength]} />
         <CaveMaterial />
@@ -94,13 +82,69 @@ const caveSideLength = 5.25;
 const caveHeight = 4;
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dtrackURI = searchParams.get("dtrack");
+  const [p, setP] = useState<[number, number, number]>([0, 0, 0]);
+  const [trackingUpdateRate, setTrackingUpdateRate] = useState<number|undefined>();
+
+  useEffect(() => {
+    if (dtrackURI) {
+      const realURI =
+        dtrackURI.startsWith("ws://") || dtrackURI.startsWith("wss://")
+        ? dtrackURI
+        : `ws://${dtrackURI}`;
+      const ws = new WebSocket(realURI);
+
+      let trackingUpdateCount = 0;
+      const trackingRateUpdateRateInterval = 250;
+
+      ws.onmessage = event => {
+        ++trackingUpdateCount;
+
+        const d = JSON.parse(event.data) as Data;
+        if (d.bodies.length > 0) {
+          if (d.bodies[0].loc) {
+            setP([
+              d.bodies[0].loc[0] / 1000,
+              d.bodies[0].loc[1] / 1000,
+              d.bodies[0].loc[2] / 1000,
+            ]);
+          }
+        }
+      }
+
+      const timer = setInterval(() => {
+        setTrackingUpdateRate(trackingUpdateCount / (trackingRateUpdateRateInterval / 1000));
+        trackingUpdateCount = 0;
+      }, trackingRateUpdateRateInterval);
+
+      return () => {
+        ws.close();
+        clearInterval(timer);
+      }
+    }
+  }, [dtrackURI]);
+
   return (
-    <Canvas>
-      <OrbitControls />
-      <Cave />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-    </Canvas>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <Cave glassPosition={p} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+      </Canvas>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      >
+        <Box>
+          Tracking Update Rate: {trackingUpdateRate}Hz
+        </Box>
+      </Box>
+    </>
   );
 }
 
